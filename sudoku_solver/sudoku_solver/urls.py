761bf7b5e535bad3e2b2c6a90309f37597ad53fe
ORORRR <v.aurore.av@gmail.com>
2019-10-29 21:43:20 +0100

    add sudoku grid style

--- sudoku_solver/sudoku_solver/urls.py
+++ sudoku_solver/sudoku_solver/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('', include('sudoku_solver_app.urls'))
+    path('', include('sudoku_solver_app.urls')),
     # path('admin/', admin.site.urls),
 ]
