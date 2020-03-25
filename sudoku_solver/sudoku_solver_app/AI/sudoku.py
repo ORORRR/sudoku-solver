db0f98e2faedb4ee24726da43f21afe830f9543e
ORORRR <v.aurore.av@gmail.com>
2020-03-26 00:56:17 +0100

    refactor sudoku lib an AI

--- sudoku_solver/sudoku_solver_app/AI/sudoku.py
+++ sudoku_solver/sudoku_solver_app/AI/sudoku.py
@@ -1,99 +1,7 @@
 from ortools.sat.python import cp_model
+from sudoku_solver_app.LIB.sudoku import is_sudoku_correct
 
-
-def is_sudoku_correct(sudoku):
-    # check that the sudoku is a 9*9 square
-    if len(sudoku) != 9:
-        return False
-    for row in sudoku:
-        if len(row) != 9:
-            return False
-
-    # check that the sudoku only contains values between 1 and 9
-    # (and 0 for the undefined boxs)
-    for row in sudoku:
-        for col in row:
-            if col < 0 or col > 9:
-                return False
-
-    # check that in each line, the numbers are all different
-    for row in sudoku:
-        for i in range(len(row)):
-            row_without_zero = list(filter((0).__ne__, row))
-            row_as_set_without_zero = set(row_without_zero)
-
-            if len(row_as_set_without_zero) != len(row_without_zero):
-                return False
-
-    # check that in each column, the numbers are all different
-    for num_col in range(len(sudoku)):
-        col = []
-        for num_row in range(len(sudoku)):
-            col.append(sudoku[num_row][num_col])
-
-        col_without_zero = list(filter((0).__ne__, col))
-        col_as_set_without_zero = set(col_without_zero)
-
-        if len(col_as_set_without_zero) != len(col_without_zero):
-            return False
-
-    # check that in each square, the numbers are all different
-    for num_row_square in range(3):
-        for num_col_square in range(3):
-            square_start_row = num_row_square * 3
-            square_start_col = num_col_square * 3
-
-            square = []
-            for row in range(square_start_row, square_start_row + 3):
-                for col in range(square_start_col, square_start_col + 3):
-                    square.append(sudoku[row][col])
-
-            square_without_zero = list(filter((0).__ne__, square))
-            square_as_set_without_zero = set(square_without_zero)
-
-            if len(square_as_set_without_zero) != len(square_without_zero):
-                return False
-
-    return True
-
-
-def is_sudoku_filled(sudoku):
-    # check that each box is filled with a value between 1 and 9
-    for row in sudoku:
-        for col in row:
-            if col < 1 or col > 9:
-                return False
-    return True
-
-
-def print_sodoku(sudoku):
-    sudoku_size = 9
-    all_rows = range(sudoku_size)
-    all_cols = range(sudoku_size)
-
-    for row in all_rows:
-        for col in all_cols:
-            if sudoku[row][col] == 0:
-                print('*', end=' ')
-            else:
-                print(sudoku[row][col], end=' ')
-            if col == 2 or col == 5:
-                print('|', end=' ')
-        print()
-        if row == 2 or row == 5:
-            for col in all_cols:
-                print('--', end='')
-                if col == 2 or col == 5:
-                    print('+', end='-')
-            print()
-
-
-def sudoku_solver(to_solve_sudoku):
-    """ solve a sudoku
-
-    Solve the sudoku given in parameter with the goold or-tools library
-    """
-
+def solve(to_solve_sudoku):
     # -------------------------------------
     #       MODEL DEFINITION
     # -------------------------------------
@@ -156,6 +64,7 @@ def sudoku_solver(to_solve_sudoku):
     # -------------------------------------
     solver = cp_model.CpSolver()
     status = solver.Solve(model)
+
     # ---------------------------------------------
     #      MAKE AND RETURN RESULT OF THE SOLVE
     # ---------------------------------------------
--- sudoku_solver/sudoku_solver_app/views.py
+++ sudoku_solver/sudoku_solver_app/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponseNotFound
 import json
-from .AI.sudoku_AI import is_sudoku_correct, sudoku_solver
-
+from .LIB.sudoku import is_sudoku_correct
+from .AI.sudoku import solve
 
 def index(request):
     if request.method == "GET":
@@ -14,20 +14,6 @@ def index(request):
     return HttpResponseNotFound('<h1>Page not found</h1>')
 
 def input_sudoku(request):
-    # sudoku = [
-    #         [0, 0, 3, 5, 7, 0, 9, 0, 0],
-    #         [0, 5, 0, 0, 0, 2, 0, 8, 0],
-    #         [7, 0, 0, 0, 0, 0, 5, 0, 1],
-        
-    #         [3, 0, 0, 0, 0, 7, 0, 5, 0],
-    #         [2, 0, 0, 0, 0, 0, 0, 0, 6],
-    #         [0, 7, 0, 2, 0, 0, 0, 0, 8],
-        
-    #         [6, 0, 4, 0, 0, 0, 0, 0, 2],
-    #         [0, 3, 0, 4, 0, 0, 0, 9, 0],
-    #         [0, 0, 7, 0, 6, 3, 4, 0, 0]
-    #     ]
-
     sudoku = [
         [0, 0, 0, 0, 0, 0, 0, 0, 0],
         [0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -43,9 +29,7 @@ def input_sudoku(request):
     ]
     
     return render(request, 'sudoku_solver_app/sudoku_input_form.html', {
-        'sudoku': sudoku,
-        'sudoku_txt': json.dumps(sudoku),
-        'sudoku_size_range': range(len(sudoku))
+        'sudoku': sudoku
     })
 
 def solve_sudoku(request):
@@ -54,14 +38,12 @@ def solve_sudoku(request):
     error = None
 
     if is_sudoku_correct(sudoku):
-        solved_sudoku = sudoku_solver(sudoku)
+        solved_sudoku = solve(sudoku)
     else:
         error = "The sudoku is not correct"
 
     return render(request, 'sudoku_solver_app/sudoku_result.html', {
         'original_sudoku': sudoku,
         'solved_sudoku': solved_sudoku['sudoku'],
-        'error': error,
-        'sudoku_txt': json.dumps(solved_sudoku['sudoku']),
-        'sudoku_size_range': range(len(sudoku))
+        'error': error
     })
