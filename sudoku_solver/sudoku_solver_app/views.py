4ba9e0fc46cf5ee1853d932ab51a673cfe047895
ORORRR <v.aurore.av@gmail.com>
2020-03-25 23:56:27 +0100

    refactor of the index view

--- sudoku_solver/sudoku_solver_app/views.py
+++ sudoku_solver/sudoku_solver_app/views.py
@@ -1,60 +1,62 @@
 from django.shortcuts import render
+from django.http import HttpResponseNotFound
 import json
 from .AI.sudoku_AI import is_sudoku_correct, sudoku_solver
 
 
 def index(request):
-    sudoku = None
+    if request.method == "GET":
+        return input_sudoku(request)
 
     if request.method == "POST":
-        sudoku = request.POST['sudoku']
+        return solve_sudoku(request)
 
-    if sudoku is None:
-        # sudoku = [
-        #     [0, 0, 3, 5, 7, 0, 9, 0, 0],
-        #     [0, 5, 0, 0, 0, 2, 0, 8, 0],
-        #     [7, 0, 0, 0, 0, 0, 5, 0, 1],
-        #
-        #     [3, 0, 0, 0, 0, 7, 0, 5, 0],
-        #     [2, 0, 0, 0, 0, 0, 0, 0, 6],
-        #     [0, 7, 0, 2, 0, 0, 0, 0, 8],
-        #
-        #     [6, 0, 4, 0, 0, 0, 0, 0, 2],
-        #     [0, 3, 0, 4, 0, 0, 0, 9, 0],
-        #     [0, 0, 7, 0, 6, 3, 4, 0, 0]
-        # ]
+    return HttpResponseNotFound('<h1>Page not found</h1>')
 
-        sudoku = [
-            [0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0],
+def input_sudoku(request):
+    # sudoku = [
+    #         [0, 0, 3, 5, 7, 0, 9, 0, 0],
+    #         [0, 5, 0, 0, 0, 2, 0, 8, 0],
+    #         [7, 0, 0, 0, 0, 0, 5, 0, 1],
+        
+    #         [3, 0, 0, 0, 0, 7, 0, 5, 0],
+    #         [2, 0, 0, 0, 0, 0, 0, 0, 6],
+    #         [0, 7, 0, 2, 0, 0, 0, 0, 8],
+        
+    #         [6, 0, 4, 0, 0, 0, 0, 0, 2],
+    #         [0, 3, 0, 4, 0, 0, 0, 9, 0],
+    #         [0, 0, 7, 0, 6, 3, 4, 0, 0]
+    #     ]
 
-            [0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    sudoku = [
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
 
-            [0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0]
-        ]
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
 
-        return render(request, 'sudoku_solver_app/sudoku_input_form.html', {
-            'sudoku': sudoku,
-            'sudoku_txt': json.dumps(sudoku),
-            'sudoku_size_range': range(len(sudoku))
-        })
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0]
+    ]
+    
+    return render(request, 'sudoku_solver_app/sudoku_input_form.html', {
+        'sudoku': sudoku,
+        'sudoku_txt': json.dumps(sudoku),
+        'sudoku_size_range': range(len(sudoku))
+    })
 
-    sudoku = json.loads(sudoku)
-
+def solve_sudoku(request):
+    sudoku = json.loads(request.POST['sudoku'])
+    solved_sudoku = None
     error = None
 
-    # check that the sudoku is correct
-    if not is_sudoku_correct(sudoku):
+    if is_sudoku_correct(sudoku):
+        solved_sudoku = sudoku_solver(sudoku)
+    else:
         error = "The sudoku is not correct"
-
-    # solve the sudoku
-
-    solved_sudoku = sudoku_solver(sudoku)
 
     return render(request, 'sudoku_solver_app/sudoku_result.html', {
         'original_sudoku': sudoku,
