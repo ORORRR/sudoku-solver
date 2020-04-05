20f5fd3e25b7ee59b3fb72a94d15cdfa9dc006b4
ORORRR <v.aurore.av@gmail.com>
2020-04-05 22:38:11 +0200

    remove useless stuff

--- sudoku_solver/sudoku_solver/urls.py
+++ sudoku_solver/sudoku_solver/urls.py
@@ -17,6 +17,5 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('', include('sudoku_solver_app.urls')),
-    # path('admin/', admin.site.urls),
+    path('', include('sudoku_solver_app.urls'))
 ]
